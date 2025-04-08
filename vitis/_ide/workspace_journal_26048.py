6174bf988b461faa1145e81295dddea778196f25
prxnav2005 <pranavmofficial2005@gmail.com>
2025-04-08 22:26:36 +0530

    vivado vitis error

--- vitis/_ide/workspace_journal_26048.py
+++
@@ -1,8 +0,0 @@
-# 2025-04-08T12:07:17.960474
-import vitis
-
-client = vitis.create_client()
-client.set_workspace(path="RTOS_Final_Vitis")
-
-vitis.dispose()
-
--- vitis/_ide/workspace_journal_8028.py
+++
@@ -1,8 +0,0 @@
-# 2025-04-08T12:13:31.223237
-import vitis
-
-client = vitis.create_client()
-client.set_workspace(path="RTOS_Final_Vitis")
-
-vitis.dispose()
-
